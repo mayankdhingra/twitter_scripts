3de02f47fee1e1c3ae6f89fcf2c74bf92c589774
Mayank Dhingra <dhingra.mayank@gmail.com>
2020-01-03 19:22:31 +0530

    added original tweet ratio and some checks on dates/username

--- select_tweets.py
+++ select_tweets.py
@@ -1,5 +1,6 @@
 import tweepy as tw
 import sys,datetime
+#from datetime import datetime
 
 auth = tw.OAuthHandler('consumer_key','consumer_secret') #insert consumer_key and consumer_secret
 auth.set_access_token('access_token','access_token_secret') #insert access_token and access_token_secret
@@ -13,26 +14,42 @@ start_date_entry = input('Enter a start date in DD-MM-YYYY format: ')
 end_date_entry   = input('Enter an end date in DD-MM-YYYY format: ')
 day, month, year = map(int, start_date_entry.split('-'))
 startDate = datetime.datetime(year, month, day,0 ,0, 0)
+#d1 = datetime.strptime(startDate, "%Y-%m-%d")
 #print(startDate)
 day, month, year = map(int, end_date_entry.split('-'))
 endDate = datetime.datetime(year, month, day, 0, 0, 0)
+duration=((endDate-startDate).days)
+
+
+#d2 = datetime.strptime(endDate, "%Y-%m-%d")
+#num_of_days = abs((d2-d1).days)
+#print(num_of_days)
 #print(endDate)
 
 # startDate = datetime.datetime(2019, 12, 1, 0, 0, 0)
-# endDate =   datetime.datetime(2019, 12, 31, 0, 0, 0)
+if duration<=0:
+	print("Please check start and end dates you entered")
+else:	
+	retweet_count=0
+	try:
+		tmpTweets = api.user_timeline(username)
+		for tweet in tmpTweets:
+		    if tweet.created_at < endDate and tweet.created_at > startDate:
+		        tweets.append(tweet)
+		        #print(tweet.text,len(tweets))
 
-tmpTweets = api.user_timeline(username)
-for tweet in tmpTweets:
-    if tweet.created_at < endDate and tweet.created_at > startDate:
-        tweets.append(tweet)
-        #print(tweet.text,len(tweets))
+		while (tmpTweets[-1].created_at > startDate):
+			tmpTweets = api.user_timeline(username, max_id=tmpTweets[-1].id)
+			for tweet in tmpTweets:
+				if tweet.created_at < endDate and tweet.created_at > startDate and tweet.id not in tweets:
+					tweets.append(tweet)
+					#print(tweet.text,len(tweets))
 
-while (tmpTweets[-1].created_at > startDate):
-	tmpTweets = api.user_timeline(username, max_id=tmpTweets[-1].id)
-	for tweet in tmpTweets:
-		if tweet.created_at < endDate and tweet.created_at > startDate and tweet.id not in tweets:
-			tweets.append(tweet)
-			#print(tweet.text,len(tweets))
-			
+		for tw in tweets:
+			if hasattr(tw, 'retweeted_status'):	
+				retweet_count+=1
 
-print(username + " has made " + str(len(tweets)) + " tweets between " + str(start_date_entry) + " and " + str(end_date_entry))
+		print(username + " has made " + str(len(tweets)) + " tweets between " + str(start_date_entry) + " and " + str(end_date_entry) + " out of which " + str(retweet_count) + " are retweets")
+		print(username	+"'s original tweet ratio is " + str(round((len(tweets)-retweet_count)/len(tweets)*100,2)) + "%") 
+	except:
+		print("Please check the username you entered")
