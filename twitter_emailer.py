425e2fd40407215975a0b6a4a03e66f87ab90e52
mayankdhingra <dhingra.mayank@gmail.com>
2020-01-26 01:58:41 +0530

    fixed formating, capturing of tweets

--- twitter_emailer.py
+++ twitter_emailer.py
@@ -9,52 +9,82 @@ auth.set_access_token('access_token','access_token_secret') #insert access_token
 api = tw.API(auth, wait_on_rate_limit=True)
 
 username = input("Enter a Twitter Username you'd like to get tweets in email for: ")
+users = username.split(',')
 
-def collect_tweets(username):
-    count=0
+#def get_tweet_text(tweet):
+#    print(tweet.truncated)
+#    if tweet.truncated:
+#        text = tweet.full_text
+#    else:
+#        text = tweet.text
+#    return text
+
+def collect_tweets(users):
     yesterdays_tweets = []
-    try:
-        tmpTweets = api.user_timeline(username)
-    except:
-        print("Please check the username you entered")
-         
-    for t in tmpTweets:
-        if t.created_at.date()+timedelta(hours=5.5) == datetime.now().date()+timedelta(days=-1):
-            if t.in_reply_to_status_id is None:
-                #print(t.text)
-                count+=1
-                yesterdays_tweets.append(t.text)
-
-    #print(str(count) + " tweets yesterday")
+    for username in users:
+        #print(username,"username")
+        count=0
+        startDate=endDate=datetime.now().date()+timedelta(days=-1)
     
+        try:
+            tmpTweets = api.user_timeline(username)
+            #results = [status._json for status in tw.Cursor(API.search, q=username, count=1000, tweet_mode='extended', lang='en').items()]
+            #print(len(results),"aaaaaa")
+        except:
+            print("Please check the username you entered")
+        
+        for t in tmpTweets:
+            #print(t.created_at," ",datetime.now().date()+timedelta(days=-1))
+            
+            if t.created_at.date()+timedelta(hours=5.5) == datetime.now().date()+timedelta(days=-1):
+                if t.in_reply_to_screen_name==username or t.in_reply_to_status_id is None:
+                    #print(t.created_at)
+                    count+=1
+                    yesterdays_tweets.append(t.text)
+        #print(tmpTweets[-1].created_at.date()+timedelta(hours=5.5) == startDate,"here",startDate)        
+        #print(type(tmpTweets[-1].created_at.date()+timedelta(hours=5.5)), type(startDate),"here",startDate)        
+        while (tmpTweets[-1].created_at.date()+timedelta(hours=5.5) == startDate):
+            #print(tmpTweets[-1].created_at,"here")
+            tmpTweets = api.user_timeline(username,max_id=tmpTweets[-1].id)
+            for tweet in tmpTweets:
+                if tweet.created_at.date()+timedelta(hours=5.5) == endDate and tweet.created_at.date()+timedelta(hours=5.5) == startDate and tweet not in yesterdays_tweets:
+                    yesterdays_tweets.append(tweet.text)
+                        
+    # for debugging missing tweets
+    #            else:
+    #                print(t.text, t.in_reply_to_screen_name, t.created_at)
+
+
+        #print(str(count) + " tweets yesterday")
+        #print("total tweets",len(yesterdays_tweets))
     return yesterdays_tweets
 
 
 def email_tweets(username,yesterdays_tweets):
+    if yesterdays_tweets:
+        sender_email = "a@gmail.com"  # Enter your address
+        receiver_email = "d@gmail.com"  # Enter receiver address
+        tweet_number=1
 
-    sender_email = "a@gmail.com"  # Enter your address
-    receiver_email = "d@gmail.com"  # Enter receiver address
+        #Subject: Yesterday's Tweets Summary: """ + str(username) + """ """ +str(len(yesterdays_tweets)) + """
+        
+        SUBJECT = "Yesterday's Tweets Summary For: " + username
+        TEXT = "\n"
+        
+        for tw in yesterdays_tweets:
+            tw = tw.encode('ascii', 'ignore').decode('ascii')
+            TEXT = TEXT + """Tweet """ +str(tweet_number) + ": "+ str(tw) + " \n"
+            tweet_number+=1
 
-    #Subject: Yesterday's Tweets Summary: """ + str(username) + """ """ +str(len(yesterdays_tweets)) + """
-    message = f"""\
-    Subject: Yesterday's Tweets Summary
-    To: {receiver_email}
-    From: {sender_email}
-    
+        print(TEXT)
 
-    """
-    for tw in yesterdays_tweets:
-        tw = tw.encode('ascii', 'ignore').decode('ascii')
-        message = message + """Tweet # """ + str(tw) + " \n"
+        message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
 
-    print(message)
-    
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login("email id", "password") #email id and password
+        server.sendmail(sender_email,receiver_email,message)
+        print("Email Sent")
+        server.quit()
 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login("email id", "password") #email id and password
-    server.sendmail(sender_email,receiver_email,message)
-
-
-
-yesterdays_tweets = collect_tweets(username)
+yesterdays_tweets = collect_tweets(users)
 email_tweets(username,yesterdays_tweets)
