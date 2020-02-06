469b47f0167efc1c2664fd048d3c692fe20c0593
mayankdhingra <dhingra.mayank@gmail.com>
2020-02-07 01:21:40 +0530

    wip to add tweet url etc

--- twitter_emailer.py
+++ twitter_emailer.py
@@ -15,9 +15,10 @@ users = username.split(',')
 
 def collect_tweets(users):
     yesterdays_tweets = []
+    yesterdays_tweets_details={} # {u1:{id1:[type1,text1,link1],id2:[type2,text2,link2]},u2:{id1:[type1,text1,link1],id2:[type2,text2,link2]}}
 
-    def get_status_text(status):
-
+    def get_status_text(status_id): 
+        status=api.get_status(status_id, tweet_mode='extended')
         if hasattr(status,'retweeted_status'):
             text = status._json['retweeted_status']['full_text']
         else:
@@ -27,27 +28,49 @@ def collect_tweets(users):
                 text = status.full_text
         return text
 
+    def get_status_url(status_id):
+        print(status_id)
+        status=api.get_status(status_id, include_entities=True)
+        print(status.entities['urls'][0]['expanded_url'])
+        status_url=status.entities['urls'][0]['expanded_url']  
+        return status_url
+
     for username in users:
-        #print(username,"username")
+        users_yday_tweets={}
         count=0
         startDate=endDate=datetime.now().date()+timedelta(days=-1)
-    
+        
         try:
-            #tmpTweets = api.user_timeline(username)
-            for status in tw.Cursor(api.user_timeline, id=username, tweet_mode='extended').items():
+            for status in tw.Cursor(api.user_timeline, id=username, tweet_mode='extended',include_entities=True).items():
                 if status.created_at.date()+timedelta(hours=5.5) == datetime.now().date()+timedelta(days=-1):
                     if status.in_reply_to_screen_name==username or status.in_reply_to_status_id is None:
                         count+=1
-                        yesterdays_tweets.append(get_status_text(status))
-            #print(len(results),"aaaaaa")
+                        #print(status.id)
+                        #print(get_status_text(status.id))
+                        #print(status.entities['urls'])
+                        #if status.entities['urls']:
+                        #    print(get_status_url(status.id))
+                        #users_yday_tweets[status.id]=[get_status_text(status.id),get_status_url(status.id)]
+                        users_yday_tweets[status.id]=[get_status_text(status.id)]
+                        #else:
+                        #    users_yday_tweets[status.id]=[get_status_text(status.id),'']
         except:
             print("Please check the username you entered")
-
-    return yesterdays_tweets
+    
+    yesterdays_tweets_details[username]=users_yday_tweets
+    #print(yesterdays_tweets_details)
+    return yesterdays_tweets_details
 
 
 def email_tweets(username,yesterdays_tweets):
-    if yesterdays_tweets:
+    #yesterdays_tweets_text=[]
+    yesterdays_tweets_text=yesterdays_tweets[username]
+    
+    #for tw in yesterdays_tweets:   
+        #yesterdays_tweets_details[tw]=get_status_text(tw)
+        #yesterdays_tweets_text.append(get_status_text(t))
+
+    if yesterdays_tweets_text:
 
         sender_email = "a@gmail.com"  # Enter your address
         receiver_email = "d@gmail.com"  # Enter receiver address
@@ -58,9 +81,10 @@ def email_tweets(username,yesterdays_tweets):
         SUBJECT = "Yesterday's Tweets Summary For:" + username
         TEXT = "\n"
         
-        for tw in list(reversed(yesterdays_tweets)):
-            tw = tw.encode('ascii', 'ignore').decode('ascii')
-            TEXT = TEXT + """Tweet """ +str(tweet_number) + ": "+ str(tw) + " \n"
+        for tw in yesterdays_tweets_text:
+            #tw = tw.encode('ascii', 'ignore').decode('ascii')
+            txt = yesterdays_tweets_text[tw][0]
+            TEXT = TEXT + """Tweet """ +str(tweet_number) + ": "+ str(txt) +  " \n"
             tweet_number+=1
 
         print(TEXT)
